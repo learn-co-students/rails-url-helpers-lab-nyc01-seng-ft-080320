163ce198a547eade0135c7c318d52f8cf0f3bad8
slurio <samantha.lurio@gmail.com>
2020-08-24 20:07:15 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.destroy_all
+
+Student.create(first_name: "Sam", last_name: "Lurio")
+Student.create(first_name: "Annika", last_name: "Lurio")
+Student.create(first_name: "Robert", last_name: "Gooden")
